db82ead5a118bbffe06405f7d9e44ba5726ee8d4
gaofeiyu <feiyu.gao@gmail.com>
2023-02-12 21:21:52 +0800

    update

---
+++ src/data/hackernews.ts
@@ -0,0 +1,545 @@
+interface IDataType {
+  name: string,
+  desc?: string,
+  link: string,
+  tags: string[],
+  date: string,
+  level?: 1 | 2 | 3 | 4 | 5
+}
+
+const d2021: IDataType[] = [
+  {
+    name: 'Blender',
+    desc: '图像渲染工具',
+    link: 'https://www.blender.org/',
+    tags: ['图像', '3D', '软件'],
+    date: '2021-12-29',
+    level: 1
+  },
+  {
+    name: 'uBlackList',
+    desc: '设置搜索黑名单',
+    link: 'https://github.com/iorate/ublacklist',
+    tags: ['chrome插件', '工具'],
+    date: '2021-12-29',
+    level: 4
+  },
+  {
+    name: 'Apache Guacamole',
+    desc: '远程桌面网关',
+    link: 'https://guacamole.apache.org/',
+    tags: ['自建服务', '工具'],
+    date: '2021-12-29',
+    level: 2
+  },
+  {
+    name: 'Y分钟速成X',
+    desc: '在线教程',
+    link: 'https://learnxinyminutes.com/',
+    tags: ['学习', '网站'],
+    date: '2021-12-29',
+    level: 3
+  },
+  {
+    name: 'NLP学习免费课程',
+    desc: '在线教程',
+    link: 'https://www.pinecone.io/learn/nlp/',
+    tags: ['学习', '网站'],
+    date: '2021-12-29',
+    level: 1
+  },
+  {
+    name: 'JAM',
+    desc: '音频社交自部署版本',
+    link: 'https://github.com/jam-systems/jam',
+    tags: ['自建服务', '社交'],
+    date: '2021-12-29',
+    level: 2
+  },
+  {
+    name: 'milk video',
+    desc: '通过视频生成声波图',
+    link: 'https://milkvideo.com/',
+    tags: ['好玩的', '工具'],
+    date: '2021-12-29',
+    level: 4
+  },
+  {
+    name: 'Alter Class',
+    desc: '在线课程开发',
+    link: 'https://alterclass.io/',
+    tags: ['自建服务', '工具'],
+    date: '2021-12-29',
+    level: 4
+  },
+  {
+    name: 'Lapce',
+    desc: '由rust开发的代码编辑器',
+    link: 'https://github.com/lapce/lapce',
+    tags: ['rust', '编辑器', '工具'],
+    date: '2021-12-29',
+    level: 4
+  },
+  {
+    name: 'Roughjs',
+    desc: '手绘风格的绘图库',
+    link: 'https://roughjs.com/',
+    tags: ['前端', 'js', '图形'],
+    date: '2021-12-29',
+    level: 3
+  },
+  {
+    name: 'messwithdns',
+    desc: '一个学习DNS原理的网站',
+    link: 'https://messwithdns.net/',
+    tags: ['服务部署', '学习', '网站'],
+    date: '2021-12-29',
+    level: 3
+  },
+  {
+    name: 'Doodle CSS',
+    desc: '手绘风格主题',
+    link: 'https://chr15m.github.io/DoodleCSS/',
+    tags: ['前端', 'css', '视觉'],
+    date: '2021-12-29',
+    level: 3
+  },
+  {
+    name: 'Paper Website',
+    desc: '将纸上的内容生成为网站',
+    link: 'https://paperwebsite.com/',
+    tags: ['好玩的', 'app'],
+    date: '2021-12-29',
+    level: 3
+  },
+  {
+    name: 'Marketing Is Scary for a Solo Developer',
+    desc: '独立开发的程序员，如何营销自己的项目',
+    link: 'https://microfounder.com/blog/marketing-is-scary',
+    tags: ['学习', '文章'],
+    date: '2021-12-29',
+    level: 4
+  },
+  {
+    name: 'Marketing Is Scary for a Solo Developer',
+    desc: '独立开发的程序员，如何营销自己的项目',
+    link: 'https://microfounder.com/blog/marketing-is-scary',
+    tags: ['学习', '文章'],
+    date: '2021-12-29',
+    level: 4
+  },
+];
+
+const d2022Q1: IDataType[] = [
+  {
+    name: '波斯王子',
+    desc: '老游戏JS移植',
+    link: 'https://princejs.com/',
+    tags: ['游戏', 'js', '好玩的'],
+    date: '2022-01-02',
+    level: 1
+  },
+  {
+    name: 'A Whole Website in a Single HTML File',
+    desc: '一个仅用HTML+CSS实现的个人博客网站',
+    link: 'https://css-tricks.com/a-whole-website-in-a-single-html-file/',
+    tags: ['前端', 'css', '好玩的'],
+    date: '2022-01-02',
+    level: 2
+  },
+  {
+    name: 'STREET FIGHTER II, PAPER TRAILS',
+    desc: '介绍在ROM比较昂贵的年代如何用有限的资源高效利用ROM',
+    link: 'https://fabiensanglard.net/sf2_sheets/',
+    tags: ['文章', '好玩的', '游戏'],
+    date: '2022-01-02',
+    level: 2
+  },
+  {
+    name: 'emfy',
+    desc: 'Emacs的配置项目',
+    link: 'https://github.com/susam/emfy',
+    tags: ['插件', 'Emacs'],
+    date: '2022-01-02',
+    level: 1
+  },
+  {
+    name: 'HTTP TOOLKIT',
+    desc: 'http请求工具包',
+    link: 'https://httptoolkit.com/',
+    tags: ['工具'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'exa',
+    desc: 'ls加强版',
+    link: 'https://github.com/ogham/exa',
+    tags: ['rust', '工具'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'Mark Text',
+    desc: 'typora开源替代品',
+    link: 'https://github.com/marktext/marktext',
+    tags: ['前端', 'js', '工具'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'tokio',
+    desc: '异步库并提供一个终端里的控制台',
+    link: 'https://github.com/tokio-rs/tokio',
+    tags: ['rust', '工具'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'python-cheatsheet',
+    desc: 'python快速查询手册',
+    link: 'https://gto76.github.io/python-cheatsheet/',
+    tags: ['学习', 'python'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'Miller',
+    desc: 'CSV, TSV, JSON, and JSON处理工具',
+    link: 'https://miller.readthedocs.io/en/latest/',
+    tags: ['数据处理', '工具'],
+    date: '2022-01-02',
+    level: 3
+  },
+  {
+    name: 'Inspect',
+    desc: 'Figma导出html或react的工具',
+    link: 'https://www.figma.com/community/plugin/1049994768493726219',
+    tags: ['插件', 'figma'],
+    date: '2022-01-02',
+    level: 2
+  },
+
+  {
+    name: 'Inspect',
+    desc: '任天堂掌上游戏机发展史回顾',
+    link: 'https://www.scanofthemonth.com/scans/game-boy-compendium',
+    tags: ['好玩的', '游戏', '文章'],
+    date: '2022-01-09',
+    level: 2
+  },
+  {
+    name: 'Why might you run your own DNS server?',
+    desc: '讲述为什么要搭建自己的DNS服务器',
+    link: 'https://jvns.ca/blog/2022/01/05/why-might-you-run-your-own-dns-server-/',
+    tags: ['服务部署', '文章'],
+    date: '2022-01-09',
+    level: 3
+  },
+  {
+    name: 'Zotero',
+    desc: '文献管理工具',
+    link: 'https://github.com/zotero/zotero',
+    tags: ['学习', '文献', '工具'],
+    date: '2022-01-09',
+    level: 3
+  },
+  {
+    name: 'Open Drop',
+    desc: '开源版的airDrop',
+    link: 'https://github.com/seemoo-lab/opendrop',
+    tags: ['python', '工具'],
+    date: '2022-01-09',
+    level: 4
+  },
+  {
+    name: 'Postmaster',
+    desc: '管理电脑上的网络情况',
+    link: 'https://safing.io/',
+    tags: ['终端', '工具'],
+    date: '2022-01-09',
+    level: 3
+  },
+  {
+    name: 'Paper We Love',
+    desc: '计算机论文资源和研读',
+    link: 'https://paperswelove.org/',
+    tags: ['计算机科学', '文献', '网站'],
+    date: '2022-01-09',
+    level: 2
+  },
+  {
+    name: 'Zig',
+    desc: '类比C和C++的底层语言',
+    link: 'https://ziglang.org/',
+    tags: ['计算机语言'],
+    date: '2022-01-09',
+    level: 2
+  },
+  {
+    name: 'No code reviews by default',
+    desc: '讨论基于信任的工程师文化',
+    link: 'https://www.raycast.com/blog/no-code-reviews-by-default',
+    tags: ['工作方式', '文章'],
+    date: '2022-01-09',
+    level: 4
+  },
+  {
+    name: '东京地铁实时可视化地图',
+    desc: '利用浏览器 web GL 实现的东京地铁实时可视化地图',
+    link: 'https://minitokyo3d.com/?utm_source=hackernewsletter&utm_medium=email&utm_term=fun',
+    tags: ['webGL', '图形'],
+    date: '2022-01-09',
+    level: 4
+  },
+
+  {
+    name: 'Browsix',
+    desc: 'Browsix 浏览器中实现 POSIX',
+    link: 'https://browsix.org/',
+    tags: ['终端', '工具'],
+    date: '2022-01-16',
+    level: 4
+  },
+  {
+    name: 'Learn and Test DMARC',
+    desc: 'Learn and Test DMARC 一个可视化工具，了解相关工作原理',
+    link: 'https://www.learndmarc.com/',
+    tags: ['学习', '网站'],
+    date: '2022-01-16',
+    level: 1
+  },
+  {
+    name: 'BookStack',
+    desc: 'BookStack 一个开源 wiki 工具',
+    link: 'https://www.bookstackapp.com/',
+    tags: ['自建服务', '工具'],
+    date: '2022-01-16',
+    level: 3
+  },
+  {
+    name: '如何在上云的服务上省钱',
+    desc: 'Poly Haven 如何花不到400美金处理80TB数据和500万次页面访问的',
+    link: 'https://blog.polyhaven.com/how-we-handle-80tb-and-5m-page-views-a-month-for-under-400/',
+    tags: ['自建服务', '文章'],
+    date: '2022-01-16',
+    level: 3
+  },
+  {
+    name: 'dsq',
+    desc: '搜索JSON, CSV, Excel等文件的CLI工具',
+    link: 'https://datastation.multiprocess.io/blog/2022-01-11-dsq.html',
+    tags: ['终端', '工具'],
+    date: '2022-01-16',
+    level: 3
+  },
+  {
+    name: 'FX',
+    desc: '在终端查看JSON内容',
+    link: 'https://github.com/antonmedv/fx',
+    tags: ['终端', '工具'],
+    date: '2022-01-16',
+    level: 4
+  },
+
+  {
+    name: 'PyFlow',
+    desc: '一个可视化编程环境',
+    link: 'https://github.com/wonderworks-software/PyFlow',
+    tags: ['python', '工具'],
+    date: '2022-01-23',
+    level: 4
+  },
+  {
+    name: 'CSS Speedrun',
+    desc: 'CSS 选择器练习题网站',
+    link: 'https://css-speedrun.netlify.app/',
+    tags: ['css', '学习', '网站'],
+    date: '2022-01-23',
+    level: 2
+  },
+  {
+    name: 'GPS',
+    desc: 'GPS 工作原理详解',
+    link: 'https://ciechanow.ski/gps/',
+    tags: ['学习', '文章'],
+    date: '2022-01-23',
+    level: 3
+  },
+  {
+    name: 'CyberChef',
+    desc: '一个数据格式转换工具',
+    link: 'https://gchq.github.io/CyberChef/',
+    tags: ['网站', '工具'],
+    date: '2022-01-23',
+    level: 3
+  },
+
+  {
+    name: 'charm',
+    desc: '用来开发CLI命令行工具的框架',
+    link: 'https://charm.sh/',
+    tags: ['终端', '框架'],
+    date: '2022-01-31',
+    level: 4
+  },
+  {
+    name: 'NOVA',
+    desc: 'mac上的代码编辑器',
+    link: 'https://nova.app/',
+    tags: ['编辑器', '工具'],
+    date: '2022-01-31',
+    level: 2
+  },
+  {
+    name: 'rnote',
+    desc: '创建手绘风格笔记的软件',
+    link: 'https://github.com/flxzt/rnote',
+    tags: ['rust', '工具'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'rnote',
+    desc: '创建手绘风格笔记的软件',
+    link: 'https://github.com/flxzt/rnote',
+    tags: ['rust', '工具'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'Github action',
+    desc: 'Github cicd服务的使用示例',
+    link: 'https://github.com/flxzt/rnote',
+    tags: ['学习', '网站'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'playwright',
+    desc: '自动测试框架',
+    link: 'https://github.com/microsoft/playwright',
+    tags: ['框架', '测试'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'prql',
+    desc: '基于rust开发的查询语言',
+    link: 'https://github.com/PRQL/prql',
+    tags: ['rust', '数据库'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'Deskreen',
+    desc: '基于JS开发的屏幕共享工具',
+    link: 'https://deskreen.com/lang-zh_CN',
+    tags: ['js', '工具'],
+    date: '2022-01-31',
+    level: 3
+  },
+  {
+    name: 'Why our team cancelled our move to microservices',
+    desc: '作者描述了他的团队违和放弃微服务的迁移',
+    link: 'https://steven-lemon182.medium.com/why-our-team-cancelled-our-move-to-microservices-8fd87898d952',
+    tags: ['微服务', '文章'],
+    date: '2022-01-31',
+    level: 3
+  },
+
+  {
+    name: 'Hibiki HTML',
+    desc: '不必使用JS的动态前端应用框架',
+    link: 'https://playground.hibikihtml.com/tutorial/',
+    tags: ['CSS', '框架'],
+    date: '2022-02-06',
+    level: 3
+  },
+  {
+    name: 'Systemd习题集',
+    desc: 'Systemd是一个常用的系统服务，但是比较难理解，这是一个Systemd的学习网站',
+    link: 'https://systemd-by-example.com/',
+    tags: ['Systemd', '学习', '网站'],
+    date: '2022-02-06',
+    level: 2
+  },
+  {
+    name: 'Malloy',
+    desc: '一个用于描述数据关系的实验性语言',
+    link: 'https://github.com/malloydata/malloy',
+    tags: ['语言', '数据库'],
+    date: '2022-02-06',
+    level: 2
+  },
+  {
+    name: 'Rich CLI',
+    desc: '命令行工具',
+    link: 'https://github.com/Textualize/rich-cli',
+    tags: ['python', '终端', '工具'],
+    date: '2022-02-06',
+    level: 2
+  },
+
+  {
+    name: 'tally',
+    desc: '快速编辑表单',
+    link: 'https://tally.so/',
+    tags: ['工具'],
+    date: '2022-02-13',
+    level: 4
+  },
+  {
+    name: 'datawrapper',
+    desc: '无需写代码，将数据转为图标、地图或表格',
+    link: 'https://www.datawrapper.de/',
+    tags: ['数据可视化', '工具'],
+    date: '2022-02-13',
+    level: 4
+  },
+  {
+    name: 'onemodel',
+    desc: '给软件工程师用的绘图工具',
+    link: 'https://www.onemodel.app/',
+    tags: ['绘图', '工具'],
+    date: '2022-02-13',
+    level: 4
+  },
+  {
+    name: 'mailwind',
+    desc: 'Use Tailwind CSS to design HTML emails.',
+    link: 'https://github.com/soheilpro/mailwind',
+    tags: ['css', '工具'],
+    date: '2022-02-13',
+    level: 3
+  },
+  {
+    name: 'backend.sql + frontend.js =♥',
+    desc: '该文章分享了与数据库交互的前端应用方式',
+    link: 'https://blog.polyglot.network/backend.sql-+-frontend.js-love',
+    tags: ['前端', '数据库', '文章'],
+    date: '2022-02-13',
+    level: 3
+  },
+  {
+    name: 'Top 10 web hacking techniques of 2021',
+    desc: '2021年的10个关于web的黑客技术',
+    link: 'https://portswigger.net/research/top-10-web-hacking-techniques-of-2021',
+    tags: ['安全', '文章'],
+    date: '2022-02-13',
+    level: 3
+  },
+  {
+    name: 'sha256algorithm',
+    desc: 'SHA256 算法可视化演示',
+    link: 'https://sha256algorithm.com/',
+    tags: ['学习', '网站'],
+    date: '2022-02-13',
+    level: 3
+  },
+];
+
+export default {
+  ...d2021,
+  ...d2022Q1
+};
